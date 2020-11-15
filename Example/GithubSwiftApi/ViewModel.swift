ddf3befe8fa8d5b2643b4ad754be70a4a9e7d858
jcleilton <josefeitosa@brq.com>
2020-11-15 00:02:58 -0300

    Image download method

--- Example/GithubSwiftApi/ViewModel.swift
+++ Example/GithubSwiftApi/ViewModel.swift
@@ -14,12 +14,15 @@ protocol ViewModelDelegate: AnyObject {
     func fetchFailled(message: String)
     func showLoading()
     func hideLoading()
+    func didLoadImage(data: Data)
 }
 
 class ViewModel {
     weak var delegate: ViewModelDelegate?
     private var data: [(id: String, value: String)] = []
     private var noMoreData = false
+    let api: GithubSwiftProtocol = GithubSwiftApi()
+    var firstImageURL = ""
     
     var count: Int {
         return self.data.count
@@ -38,10 +41,10 @@ class ViewModel {
         }
         self.delegate?.showLoading()
         let page = self.data.count == 0 ? "1" : "\((self.data.count/10) + 1)"
-        let api = GithubSwiftApi()
         api.fetch(page: page) { [weak self] result in
             switch result {
             case .success(let data):
+                self?.firstImageURL = data.items.first?.owner.avatarURL ?? ""
                 self?.data += data.items.compactMap{ (id: "\($0.id)", value: $0.name) }
                 self?.delegate?.didLoad()
             case .failure(let error):
@@ -61,5 +64,17 @@ class ViewModel {
         self.data = []
         self.noMoreData = false
         self.fetchData()
+    }
+    
+    func getImage() {
+        let apiMock = self.firstImageURL.isEmpty ? GithubSwiftMockApi() : self.api
+        apiMock.getImage(url: self.firstImageURL) { [weak self] result in
+            switch result {
+            case .success(let data):
+                self?.delegate?.didLoadImage(data: data)
+            case .failure(_):
+                break
+            }
+        }
     }
 }
--- src/protocols/GithubSwiftProtocol.swift
+++ src/protocols/GithubSwiftProtocol.swift
@@ -9,4 +9,5 @@ import Foundation
 
 public protocol GithubSwiftProtocol {
     func fetch(page: String, completion: @escaping(Result<GithubSwiftResponse, GithubSwiftError>) -> Void)
+    func getImage(url: String, completion: @escaping (Result<Data, GithubSwiftError>) -> Void)
 }
